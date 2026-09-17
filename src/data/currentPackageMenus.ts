b0248f4130090ca1865e973ceba878e892be6ca6
rnsingh28rs-arch <r.n.singh28.rs@gmail.com>
2026-09-18 01:09:51 +0530

    feat: centralize current weekly package menus

---
+++ src/data/currentPackageMenus.ts
@@ -0,0 +1,31 @@
+import type { DayMenuSchedule } from '../types';
+
+export const CURRENT_VEG_CLASSIC_MENU: DayMenuSchedule[] = [
+  { day: 'Monday', lunch: { dal: 'Dal Tadka', dryVeg: 'Aloo Jeera Bhujia', gravyOrNonVeg: '', rice: 'Steamed Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' }, dinner: { dal: '', dryVeg: 'Aloo Beans Bhujia', gravyOrNonVeg: 'Rajma Masala', rice: 'Steamed Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' } },
+  { day: 'Tuesday', lunch: { dal: 'Chana Dal Tadka', dryVeg: 'Bhindi Masala Dry', gravyOrNonVeg: '', rice: 'Jeera Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' }, dinner: { dal: '', dryVeg: 'Aloo Capsicum Bhujia', gravyOrNonVeg: 'Punjabi Chole Masala', rice: 'Steamed Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' } },
+  { day: 'Wednesday', lunch: { dal: 'Moong-Masoor Dal', dryVeg: '', gravyOrNonVeg: 'Paneer Bhurji', rice: 'Steamed Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' }, dinner: { dal: 'Mix Dal Fry', dryVeg: 'Aloo Gobhi Bhujia', gravyOrNonVeg: '', rice: 'Steamed Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' } },
+  { day: 'Thursday', lunch: { dal: 'Dal Palak', dryVeg: 'Aloo Matar Bhujia', gravyOrNonVeg: '', rice: 'Jeera Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' }, dinner: { dal: '', dryVeg: 'Aloo Jeera Bhujia', gravyOrNonVeg: 'Shahi Paneer', rice: 'Steamed Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' } },
+  { day: 'Friday', lunch: { dal: 'Punjabi Kadhi Pakoda', dryVeg: 'Aloo Beans Bhujia', gravyOrNonVeg: '', rice: 'Steamed Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' }, dinner: { dal: 'Masoor Dal Tadka', dryVeg: 'Mix Veg Bhujia', gravyOrNonVeg: '', rice: 'Jeera Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' } },
+  { day: 'Saturday', lunch: { dal: 'Dal Fry', dryVeg: 'Aloo Gobhi Bhujia', gravyOrNonVeg: '', rice: 'Steamed Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' }, dinner: { dal: '', dryVeg: 'Beans-Carrot Bhujia', gravyOrNonVeg: 'Paneer Butter Masala', rice: 'Jeera Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' } },
+  { day: 'Sunday', lunch: { dal: 'Dal Makhani', dryVeg: 'Punjabi Aloo Bhujia', gravyOrNonVeg: '', rice: 'Veg Pulao', foilPacked: '4 Roti', extras: 'Salad & Achar' }, dinner: null }
+];
+
+export const CURRENT_EGG_DELIGHT_MENU: DayMenuSchedule[] = [
+  { day: 'Monday', lunch: { dal: '', dryVeg: '', gravyOrNonVeg: '', rice: '', foilPacked: '', extras: '' }, dinner: { dal: '', dryVeg: 'Aloo Beans Bhujia', gravyOrNonVeg: 'Egg Curry (2 Eggs)', rice: 'Steamed Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' } },
+  { day: 'Tuesday', lunch: { dal: '', dryVeg: '', gravyOrNonVeg: '', rice: '', foilPacked: '', extras: '' }, dinner: { dal: '', dryVeg: 'Aloo Capsicum Bhujia', gravyOrNonVeg: 'Punjabi Chole Masala', rice: 'Steamed Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' } },
+  { day: 'Wednesday', lunch: { dal: '', dryVeg: '', gravyOrNonVeg: '', rice: '', foilPacked: '', extras: '' }, dinner: { dal: '', dryVeg: 'Aloo Gobhi Bhujia', gravyOrNonVeg: 'Egg Masala Curry (2 Eggs)', rice: 'Steamed Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' } },
+  { day: 'Thursday', lunch: { dal: '', dryVeg: '', gravyOrNonVeg: '', rice: '', foilPacked: '', extras: '' }, dinner: { dal: '', dryVeg: 'Aloo Jeera Bhujia', gravyOrNonVeg: 'Shahi Paneer', rice: 'Steamed Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' } },
+  { day: 'Friday', lunch: { dal: '', dryVeg: '', gravyOrNonVeg: '', rice: '', foilPacked: '', extras: '' }, dinner: { dal: 'Masoor Dal Tadka', dryVeg: 'Mix Veg Bhujia', gravyOrNonVeg: '', rice: 'Jeera Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' } },
+  { day: 'Saturday', lunch: { dal: '', dryVeg: '', gravyOrNonVeg: '', rice: '', foilPacked: '', extras: '' }, dinner: { dal: '', dryVeg: 'Beans-Carrot Bhujia', gravyOrNonVeg: 'Egg Bhurji', rice: 'Jeera Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' } },
+  { day: 'Sunday', lunch: { dal: '', dryVeg: '', gravyOrNonVeg: '', rice: '', foilPacked: '', extras: '' }, dinner: null }
+];
+
+export const CURRENT_NON_VEG_CLUB_MENU: DayMenuSchedule[] = [
+  { day: 'Monday', lunch: { dal: '', dryVeg: '', gravyOrNonVeg: '', rice: '', foilPacked: '', extras: '' }, dinner: { dal: '', dryVeg: 'Aloo Beans Bhujia', gravyOrNonVeg: 'Rajma Masala', rice: 'Steamed Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' } },
+  { day: 'Tuesday', lunch: { dal: '', dryVeg: '', gravyOrNonVeg: '', rice: '', foilPacked: '', extras: '' }, dinner: { dal: '', dryVeg: 'Aloo Capsicum Bhujia', gravyOrNonVeg: 'Chicken Curry (3 pcs)', rice: 'Steamed Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' } },
+  { day: 'Wednesday', lunch: { dal: '', dryVeg: '', gravyOrNonVeg: '', rice: '', foilPacked: '', extras: '' }, dinner: { dal: 'Mix Dal Fry', dryVeg: 'Aloo Gobhi Bhujia', gravyOrNonVeg: '', rice: 'Steamed Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' } },
+  { day: 'Thursday', lunch: { dal: '', dryVeg: '', gravyOrNonVeg: '', rice: '', foilPacked: '', extras: '' }, dinner: { dal: '', dryVeg: 'Aloo Jeera Bhujia', gravyOrNonVeg: 'Chicken Masala (3 pcs)', rice: 'Steamed Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' } },
+  { day: 'Friday', lunch: { dal: '', dryVeg: '', gravyOrNonVeg: '', rice: '', foilPacked: '', extras: '' }, dinner: { dal: '', dryVeg: 'Mix Veg Bhujia', gravyOrNonVeg: 'Chicken Curry (3 pcs)', rice: 'Jeera Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' } },
+  { day: 'Saturday', lunch: { dal: '', dryVeg: '', gravyOrNonVeg: '', rice: '', foilPacked: '', extras: '' }, dinner: { dal: 'Panchmel Dal', dryVeg: 'Beans-Carrot Bhujia', gravyOrNonVeg: 'Paneer Butter Masala', rice: 'Steamed Rice', foilPacked: '4 Roti', extras: 'Salad & Achar' } },
+  { day: 'Sunday', lunch: { dal: '', dryVeg: '', gravyOrNonVeg: '', rice: '', foilPacked: '', extras: '' }, dinner: null }
+];
